e3650c5efa37c828b79391e0ef40917c30138813
MoonGoon <moongoon.cnu@gmail.com>
2023-02-08 15:57:47 +0900

    ✅ Update: 쓸모 없는 부분 삭제

--- 02_08/[누적 합] 최대 구간 합/MaximumSubArray1.py
+++ 02_08/[누적 합] 최대 구간 합/MaximumSubArray1.py
@@ -1,11 +1,6 @@
 n = int(input())
 sequences = list(map(int, input().split(' ')))
 
-prefixSum = 0
-prefixArr = []
-for seq in sequences:
-    prefixSum += seq
-    prefixArr.append(prefixSum)
 result = -100
 pSum = 0
 for i in range(n):
