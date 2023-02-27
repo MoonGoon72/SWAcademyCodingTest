b365fbd5ff93223eb1695531e6a0784e8c923c17
MoonGoon <moongoon.cnu@gmail.com>
2023-02-27 13:26:05 +0900

    ✅ Solve: DFS

---
+++ 02_27/DFS/DFS.py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split(' '))
+graph = [[] for _ in range(n + 1)]
+visited = [False for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split(' '))
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(start):
+    stack = [start]
+    while stack:
+        now = stack.pop()
+        if not visited[now]: print(now, end=' ')
+        visited[now] = True
+        graph[now].sort(reverse=True)
+        for i in graph[now]:
+            if not visited[i]:
+                stack.append(i)
+
+dfs(1)
